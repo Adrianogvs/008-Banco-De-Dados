0d6946a82450ed02f85883f9ff6fa4867375f42d
Adriano Vilela <98471300+Adrianogvs@users.noreply.github.com>
2022-10-21 00:32:42 -0300

    Curso SQL Server.

---
+++ 001-SQL Server/repos/002-Indrodução a SQL/009-Exercicio 4.sql
@@ -0,0 +1,12 @@
+/*
+4. A empresa Contoso precisa fazer contato com os fornecedores de produtos para repor o estoque. 
+Você É da Área de compras e precisa descobrir quem sÃo esses fornecedores.
+Utilize um comando em SQL para retornar apenas os nomes dos fornecedores na tabela dimProduct e renomeie essa nova coluna da tabela
+
+*/
+
+-- CONECTANDO NO BANCO DE DADOS
+USE ContosoRetailDW
+
+SELECT	* 
+FROM DimProduct;
