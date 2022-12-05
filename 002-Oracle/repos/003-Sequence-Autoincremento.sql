c5c9f72305941fc622cc38e089e4fbc0d0d509db
Adriano Vilela <98471300+Adrianogvs@users.noreply.github.com>
2022-12-05 20:11:30 -0300

    Curso Banco de Dados Oracle

---
+++ 002-Oracle/repos/003-Sequence-Autoincremento.sql
@@ -0,0 +1,30 @@
+-- SEQUENCE E AUTOINCREMENTO
+
+-- O QUE SÃO SEQUENCE?
+/*
+Sequences são objetos que geram números sequenciais dentro do Oracle. 
+São excelentes em termos de performance já que armazenam um range de números no cache e também porque tiram do desenvolvedor a responsabilidade de gerenciar a geração de Ids em casos de multiplos acessos.
+
+Para criar uma sequence usamos a sintaxe abaixo.
+
+CREATE SEQUENCE sqTeste
+    MINVALUE 1
+    MAXVALUE 300
+    START WITH 1
+    INCREMENT BY 1
+    CACHE 20;
+
+* INCREMENT BY: Informa ao sistema como incrementar a sequence, pode receber valores positivos e negatives. Se o valor informado for positivo a sequence será ascendente, se o valor for negativo a sequence será decrescente.
+* START WITH: Informa ao sistema o valor inteiro com o qual a sequence iniciará.
+* MINVALUE: Informa ao sistema o menor valor que uma sequence pode ter. Normalmente o valor desta propriedade é o mesmo valor da propriedade Start With.
+* MAXVALUE: Informa ao sistema o maior valor que uma sequence pode assumir.
+* CACHE: Especifica o número de valores da sequence que ficarão armazenados. Ou seja, só haverá processamento para a recuperação de valores, quando o range de valores armazenados no cache acabar.
+
+Agora que você já sabe como criar um sequence, vamos ver o que eu considero o seu principal uso, fazer um Insert em uma tabela que possui um campo numérico como chave primária.
+
+INSERT INTO CLIENTE
+ (ID, NOME)
+ VALUES
+ (sqTeste.nextval, 'Adonirandina')
+*/
+
